c090086203f571f73c8b88b7597a16f71d561ac1
Calvin Lang <langcalvin@gmail.com>
2022-04-21 14:47:52 +0800

    fixed dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export { default as appVersion } from './appVersion';
-export { default as filterObjectsByKey } from './filterObjectByKeys';
-export * from './genLinks';
-export { default as pNum } from './pNum';
-export { default as runAsync } from './runAsync';
-export const keyForTesting = process.env.REACT_APP_ENVIRONMENT || '';
+export { default as appVersion } from './appVersion'
+export { default as filterObjectsByKey } from './filterObjectByKeys'
+export * from './genLinks'
+export { default as pNum } from './pNum'
+export { default as runAsync } from './runAsync'
--- src/runAsync.ts
+++ src/runAsync.ts
@@ -1,13 +1,13 @@
 export default function runAsync(callback: Function) {
-  let result: any;
+  let result: any
   return new Promise(async (res, rej) => {
     await callback()
-      .then((r) => {
-        console.log({ r });
-        res(r);
-        return r.json();
+      .then((r: any) => {
+        console.log({ r })
+        res(r)
+        return r.json()
       })
-      .catch((err) => rej(err));
-  });
+      .catch((err: unknown) => rej(err))
+  })
   // if (result) return result;
 }
